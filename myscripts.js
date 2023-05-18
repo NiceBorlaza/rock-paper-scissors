14507eda5ca6743d2de676849080a57804a9dfd1
Nice Borlaza <112726227+NiceBorlaza@users.noreply.github.com>
2023-05-18 21:56:31 +0800

    modified index.html and myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -1,9 +1,8 @@
-const playerSelection = ''
-const computerSelection = ''
 let result = ''
 let computerScore = 0
 let playerScore = 0
-
+let round = 0
+const outcomeDiv = document.querySelector(".outcomeDiv")
 
 function getComputerChoice() {
     const choices = ["rock", "paper", "scissor"]
@@ -12,68 +11,91 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
-    playerSelection = prompt("Enter your pick")
+    console.log("1", playerSelection, "2", computerSelection)
     computerSelection = getComputerChoice()
     if (playerSelection === computerSelection) {
-        result = "tie"
+        const p = document.createElement("p")
+        p.innerText = "tie"
+        outcomeDiv.appendChild(p)
+
     }
     else if (playerSelection === "rock" && computerSelection === "scissor") {
-        result = "player scores";
+        const p = document.createElement("p")
+        p.innerText = "rock beats scissors";
         playerScore = playerScore + 1
+        outcomeDiv.appendChild(p)
     }
     else if (playerSelection === "rock" && computerSelection === "paper") {
-        result = "computer scores"
+        const p = document.createElement("p")
+        p.innerText = "paper beats rock"
         computerScore += 1
+        outcomeDiv.appendChild(p)
     }
     else if (playerSelection === "paper" && computerSelection === "rock") {
-        result = "player scores";
+        const p = document.createElement("p")
+        p.innerText = "paper beats scissors";
         playerScore = playerScore + 1
+        outcomeDiv.appendChild(p)
     }
     else if (playerSelection === "paper" && computerSelection === "scissor") {
-        result = "computer scores";
+        const p = document.createElement("p")
+        p.innerText = "scissors beats rock";
         computerScore += 1
+        outcomeDiv.appendChild(p)
     }
     else if (playerSelection === "scissor" && computerSelection === "paper") {
-        result = "player scores";
+        const p = document.createElement("p")
+        p.innerText = "scissors beats paper";
         playerScore = playerScore + 1
+        outcomeDiv.appendChild(p)
     }
     else if (playerSelection === "scissor" && computerSelection === "rock") {
-        result = "computer scores";
+        const p = document.createElement("p")
+        p.innerText = "rock beats scissors";
         computerScore += 1
-    }
-    return result 
+        outcomeDiv.appendChild(p)
+    } 
 
 }
 
-function game() {
-    console.log("round result " + playRound(playerSelection, computerSelection))
-    console.log("player score " + playerScore)
-    console.log("computer score " + computerScore)
-    console.log("round result " + playRound(playerSelection, computerSelection))
-    console.log("player score " + playerScore)
-    console.log("computer score " + computerScore)
-    console.log("round result " + playRound(playerSelection, computerSelection))
-    console.log("player score " + playerScore)
-    console.log("computer score " + computerScore)
-    console.log("round result " + playRound(playerSelection, computerSelection))
-    console.log("player score " + playerScore)
-    console.log("computer score " + computerScore)
-    console.log("round result " + playRound(playerSelection, computerSelection))
-    console.log("player score " + playerScore)
-    console.log("computer score " + computerScore)
-
-    if (playerScore > computerScore) {
-        console.log("player wins the game")
-    }
-    else if (playerScore < computerScore) {
-        console.log("computer wins the game")
-    }
-    else {
-        console.log("tie game")
+function scores() {
+    const p1 = document.createElement("p")
+    p1.textContent = "Player Score: " + playerScore
+    outcomeDiv.appendChild(p1)
+    const p2 = document.createElement("p")
+    p2.textContent = "Computer Score: " + computerScore
+    outcomeDiv.appendChild(p2)
+    if (playerScore === 5 ) {
+        const p3 = document.createElement("p")
+        p3.textContent = "player beats computer"
+        outcomeDiv.appendChild(p3)
+    }else if (computerScore === 5) {
+        const p3 = document.createElement("p")
+        p3.textContent = "computer beats computer"
+        outcomeDiv.appendChild(p3)
     }
 }
-game()
 
+const rockBtn = document.querySelector("#rockBtn");
+rockBtn.addEventListener("click", () => {
+    computerSelection = getComputerChoice()
+    playerSelection = rockBtn.textContent
+    playRound(playerSelection, computerSelection)
+    scores();
+});
 
+const paperBtn = document.querySelector("#paperBtn");
+paperBtn.addEventListener("click", () => {
+    computerSelection = getComputerChoice()
+    playerSelection = paperBtn.textContent
+    playRound(playerSelection, computerSelection)
+    scores();
+});
 
-
+const scissorBtn = document.querySelector("#scissorBtn");
+scissorBtn.addEventListener("click", () => {
+    computerSelection = getComputerChoice()
+    playerSelection = scissorBtn.textContent
+    playRound(playerSelection, computerSelection)
+    scores();
+});
